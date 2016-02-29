a4d83f1742494cc97348a25e4b11f310bd7bb398
François Legastelois <flegastelois@teclib.com>
2016-02-29 15:00:00 +0100

    fix kimios admin module config page

--- htdocs/kimios/admin/config.php
+++ htdocs/kimios/admin/config.php
@@ -26,10 +26,20 @@
  @since     2013
  ---------------------------------------------------------------------- */
  
-require '../../../main.inc.php';
+$res=0;
+if (! $res && ! empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res=@include($_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php");
+if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
+if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
+if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
+if (! $res && file_exists("../../../../main.inc.php")) $res=@include("../../../../main.inc.php");
+if (! $res && file_exists("../../../../../main.inc.php")) $res=@include("../../../../../main.inc.php");
+if (! $res && preg_match('/\/nltechno([^\/]*)\//',$_SERVER["PHP_SELF"],$reg)) $res=@include("../../../../dolibarr".$reg[1]."/htdocs/main.inc.php"); // Used on dev env only
+if (! $res && preg_match('/\/teclib([^\/]*)\//',$_SERVER["PHP_SELF"],$reg)) $res=@include("../../../../dolibarr".$reg[1]."/htdocs/main.inc.php"); // Used on dev env only
+if (! $res) die("Include of main fails");
+
 require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
-require_once DOL_DOCUMENT_ROOT.'/kimios/class/db.class.php';
-require_once DOL_DOCUMENT_ROOT.'/kimios/class/config.class.php';
+dol_include_once('/kimios/class/db.class.php');
+dol_include_once('/kimios/class/config.class.php');
 
 $langs->load("admin");
 $langs->load("kimios@kimios");
