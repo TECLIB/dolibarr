558ab216e1c5fd2b3f503d8e8c0b385379e45bb2
Laurent Destailleur <eldy@destailleur.fr>
2017-06-29 13:56:07 +0200

    Fix replacement of project ref into invoice number

--- htdocs/forceproject/core/triggers/interface_50_modProjet_ForceProject.class.php
+++ htdocs/forceproject/core/triggers/interface_50_modProjet_ForceProject.class.php
@@ -140,7 +140,7 @@ class InterfaceForceProject
 	            	$newref=preg_replace('/\{PROJECTREF\-[1-9]\}/',$projectref,$newref);  // When mask is  ...{PROJECTREF-9}... for example
 	            	$newref=preg_replace('/%%+/',$projectref,$newref);
 
-	            	// If we want counter is started to 1 for each project
+	            	// If we want the counter to start to 1 for each project
 	            	if (! empty($conf->global->FORCEPROJECT_COUNTER_FOREACH_PROJECT))
 	            	{
 		            	$savmask=$conf->global->PROPALE_SAPHIR_MASK;
@@ -204,7 +204,7 @@ class InterfaceForceProject
 	            	$newref=preg_replace('/\{PROJECTREF\-[1-9]\}/',$projectref,$newref);  // When mask is  ...{PROJECTREF-9}... for example
 	            	$newref=preg_replace('/%%+/',$projectref,$newref);
 
-	            	// If we want counter is started to 1 for each project
+	            	// If we want the counter to start to 1 for each project
 	            	if (! empty($conf->global->FORCEPROJECT_COUNTER_FOREACH_PROJECT))
 	            	{
 		            	$savmask=$conf->global->COMMANDE_SAPHIR_MASK;
@@ -243,7 +243,7 @@ class InterfaceForceProject
         {
             dol_syslog("Trigger '".$this->name."' for action '$action' launched by ".__FILE__.". id=".$object->id);
 
-            if (empty($object->fk_project))
+            if (empty($object->fk_project) || $object->fk_project < 0)
             {
 				$langs->load("forceproject@forceproject");	// So files is loaded for function to show error message
             	$this->errors[]=$langs->trans("InvoiceMustBeLinkedToProject");
@@ -256,6 +256,7 @@ class InterfaceForceProject
                 $projectref=$object->projet->ref;
 
                 $sql="SELECT facnumber as ref FROM ".MAIN_DB_PREFIX."facture WHERE rowid=".$object->id;
+
                 $resql=$this->db->query($sql);
                 if ($resql)
                 {
@@ -265,37 +266,55 @@ class InterfaceForceProject
                     $newref=preg_replace('/\{PROJECTREF\-[1-9]\}/',$projectref,$newref);  // When mask is  ...{PROJECTREF-9}... for example
                     $newref=preg_replace('/%%+/',$projectref,$newref);
 
-                    // If we want counter is started to 1 for each project
+                    // If we want the counter to start to 1 for each project
                     if (! empty($conf->global->FORCEPROJECT_COUNTER_FOREACH_PROJECT))
                     {
-                        $savmask=$conf->global->FACTURE_MERCURE_MASK_INVOICE;
-                        $conf->global->FACTURE_MERCURE_MASK_INVOICE=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_INVOICE);
-                        $newref=$object->getNextNumRef($object->thirdparty);
-                        //$newref=$projectref.substr($newref,7);
-                        $conf->global->FACTURE_MERCURE_MASK_INVOICE=$savmask;
-                        //var_dump($newref); exit;
-
-                        $savmask=$conf->global->FACTURE_MERCURE_MASK_REPLACEMENT;
-                        $conf->global->FACTURE_MERCURE_MASK_REPLACEMENT=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_REPLACEMENT);
-                        $newref=$object->getNextNumRef($object->thirdparty);
-                        //$newref=$projectref.substr($newref,7);
-                        $conf->global->FACTURE_MERCURE_MASK_REPLACEMENT=$savmask;
-                        //var_dump($newref); exit;
-
-                        $savmask=$conf->global->FACTURE_MERCURE_MASK_CREDIT;
-                        $conf->global->FACTURE_MERCURE_MASK_CREDIT=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_CREDIT);
-                        $newref=$object->getNextNumRef($object->thirdparty);
-                        //$newref=$projectref.substr($newref,7);
-                        $conf->global->FACTURE_MERCURE_MASK_CREDIT=$savmask;
-                        //var_dump($newref); exit;
-
-                        $savmask=$conf->global->FACTURE_MERCURE_MASK_DEPOSIT;
-                        $conf->global->FACTURE_MERCURE_MASK_DEPOSIT=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_DEPOSIT);
-                        $newref=$object->getNextNumRef($object->thirdparty);
-                        //$newref=$projectref.substr($newref,7);
-                        $conf->global->FACTURE_MERCURE_MASK_DEPOSIT=$savmask;
-                        //var_dump($newref); exit;
+                        if ($object->type == 1)
+                        {
+                            $savmask=$conf->global->FACTURE_MERCURE_MASK_REPLACEMENT;
+                            $conf->global->FACTURE_MERCURE_MASK_REPLACEMENT=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_REPLACEMENT);
+                            $conf->global->FACTURE_MERCURE_MASK_REPLACEMENT=preg_replace('/\{PROJECTREF\-[1-9]\}/',$projectref,$conf->global->FACTURE_MERCURE_MASK_REPLACEMENT);
+                            $conf->global->FACTURE_MERCURE_MASK_REPLACEMENT=preg_replace('/%%+/',$projectref,$conf->global->FACTURE_MERCURE_MASK_REPLACEMENT);
+                            $newref=$object->getNextNumRef($object->thirdparty);
+                            //$newref=$projectref.substr($newref,7);
+                            $conf->global->FACTURE_MERCURE_MASK_REPLACEMENT=$savmask;
+                            //var_dump($newref); exit;
+                        }
+                        elseif ($object->type == 2)
+                        {
+                            $savmask=$conf->global->FACTURE_MERCURE_MASK_CREDIT;
+                            $conf->global->FACTURE_MERCURE_MASK_CREDIT=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_CREDIT);
+                            $conf->global->FACTURE_MERCURE_MASK_CREDIT=preg_replace('/\{PROJECTREF\-[1-9]\}/',$projectref,$conf->global->FACTURE_MERCURE_MASK_CREDIT);
+                            $conf->global->FACTURE_MERCURE_MASK_CREDIT=preg_replace('/%%+/',$projectref,$conf->global->FACTURE_MERCURE_MASK_CREDIT);
+                            $newref=$object->getNextNumRef($object->thirdparty);
+                            //$newref=$projectref.substr($newref,7);
+                            $conf->global->FACTURE_MERCURE_MASK_CREDIT=$savmask;
+                            //var_dump($newref); exit;
+                        }
+                        elseif ($object->type == 3)
+                        {
+                            $savmask=$conf->global->FACTURE_MERCURE_MASK_DEPOSIT;
+                            $conf->global->FACTURE_MERCURE_MASK_DEPOSIT=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_DEPOSIT);
+                            $conf->global->FACTURE_MERCURE_MASK_DEPOSIT=preg_replace('/\{PROJECTREF\-[1-9]\}/',$projectref,$conf->global->FACTURE_MERCURE_MASK_DEPOSIT);
+                            $conf->global->FACTURE_MERCURE_MASK_DEPOSIT=preg_replace('/%%+/',$projectref,$conf->global->FACTURE_MERCURE_MASK_DEPOSIT);
+                            $newref=$object->getNextNumRef($object->thirdparty);
+                            //$newref=$projectref.substr($newref,7);
+                            $conf->global->FACTURE_MERCURE_MASK_DEPOSIT=$savmask;
+                            //var_dump($newref); exit;
+                        }
+                        else
+                        {
+                            $savmask=$conf->global->FACTURE_MERCURE_MASK_INVOICE;
+                            $conf->global->FACTURE_MERCURE_MASK_INVOICE=preg_replace('/projectref/',$projectref,$conf->global->FACTURE_MERCURE_MASK_INVOICE);
+                            $conf->global->FACTURE_MERCURE_MASK_INVOICE=preg_replace('/\{PROJECTREF\-[1-9]\}/',$projectref,$conf->global->FACTURE_MERCURE_MASK_INVOICE);
+                            $conf->global->FACTURE_MERCURE_MASK_INVOICE=preg_replace('/%%+/',$projectref,$conf->global->FACTURE_MERCURE_MASK_INVOICE);
+                            $newref=$object->getNextNumRef($object->thirdparty);
+                            //$newref=$projectref.substr($newref,7);
+                            $conf->global->FACTURE_MERCURE_MASK_INVOICE=$savmask;
+                            //var_dump($newref); exit;
+                        }
                     }
+
                     dol_syslog("We validate order ".$object->id." oldref=".$object->ref." newref=".$newref." projectid=".$projectid." projectref=".$projectref);
 
                     $sql="UPDATE ".MAIN_DB_PREFIX."facture SET facnumber = '".$this->db->escape($newref)."' WHERE rowid=".$object->id;
