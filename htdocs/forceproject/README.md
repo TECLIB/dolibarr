9ce239c41359954da0c5445fc8cff19c357e1268
Laurent Destailleur <eldy@destailleur.fr>
2018-03-12 18:05:05 +0100

    FIX Can set FORCEPROJECT_COUNTER_FOREACH_PROJECT and avoid increase of ref number by step of 2

---
+++ htdocs/forceproject/README.md
@@ -0,0 +1,25 @@
+# FORCEPROJECT FOR <a href="https://www.dolibarr.org">DOLIBARR ERP CRM</a>
+
+## Features
+
+Check a project is linked to allow to validate proposal or order. 
+It may also replace key '{PROJECTREF\-X}' with ref of linked project if found into ref (X is number of chars of project ref
+
+
+
+Licenses
+--------
+
+### Main code
+
+![GPLv3 logo](img/gplv3.png)
+
+GPLv3 or (at your option) any later version.
+
+See [COPYING](COPYING) for more information.
+
+#### Documentation
+
+All texts and readmes.
+
+![GFDL logo](img/gfdl.png)
--- htdocs/forceproject/core/modules/modForceProject.class.php
+++ htdocs/forceproject/core/modules/modForceProject.class.php
@@ -55,7 +55,7 @@ class modForceProject extends DolibarrModules
         $this->editor_name = 'TecLib';
         $this->editor_url = 'http://www.teclib.com';
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
