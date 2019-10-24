7e84916f55790768767257b5dc502de5249e3a8c
Laurent Destailleur <eldy@destailleur.fr>
2019-10-24 14:30:43 +0200

    Add dictionary of type of documents

--- htdocs/justificativedocuments/core/modules/modJustificativeDocuments.class.php
+++ htdocs/justificativedocuments/core/modules/modJustificativeDocuments.class.php
@@ -180,30 +180,27 @@ class modJustificativeDocuments extends DolibarrModules
         // 'user'             to add a tab in user view
 
         // Dictionaries
-        $this->dictionaries=array();
-        /* Example:
         $this->dictionaries=array(
-            'langs'=>'mylangfile@justificativedocuments',
+            'langs'=>'justificativedocuments@justificativedocuments',
             // List of tables we want to see into dictonnary editor
-            'tabname'=>array(MAIN_DB_PREFIX."table1",MAIN_DB_PREFIX."table2",MAIN_DB_PREFIX."table3"),
+            'tabname'=>array(MAIN_DB_PREFIX."c_justificative_type"),
             // Label of tables
-            'tablib'=>array("Table1","Table2","Table3"),
+            'tablib'=>array("TypeOfJustificative"),
             // Request to select fields
-            'tabsql'=>array('SELECT f.rowid as rowid, f.code, f.label, f.active FROM '.MAIN_DB_PREFIX.'table1 as f','SELECT f.rowid as rowid, f.code, f.label, f.active FROM '.MAIN_DB_PREFIX.'table2 as f','SELECT f.rowid as rowid, f.code, f.label, f.active FROM '.MAIN_DB_PREFIX.'table3 as f'),
+            'tabsql'=>array('SELECT f.rowid as rowid, f.code, f.label, f.active FROM '.MAIN_DB_PREFIX.'c_justificative_type as f'),
             // Sort order
-            'tabsqlsort'=>array("label ASC","label ASC","label ASC"),
+            'tabsqlsort'=>array("label ASC"),
             // List of fields (result of select to show dictionary)
-            'tabfield'=>array("code,label","code,label","code,label"),
+            'tabfield'=>array("code,label"),
             // List of fields (list of fields to edit a record)
-            'tabfieldvalue'=>array("code,label","code,label","code,label"),
+            'tabfieldvalue'=>array("code,label"),
             // List of fields (list of fields for insert)
-            'tabfieldinsert'=>array("code,label","code,label","code,label"),
+            'tabfieldinsert'=>array("code,label"),
             // Name of columns with primary key (try to always name it 'rowid')
-            'tabrowid'=>array("rowid","rowid","rowid"),
+            'tabrowid'=>array("rowid"),
             // Condition to show each dictionary
-            'tabcond'=>array($conf->justificativedocuments->enabled,$conf->justificativedocuments->enabled,$conf->justificativedocuments->enabled)
+            'tabcond'=>array($conf->justificativedocuments->enabled)
         );
-        */
 
         // Boxes/Widgets
         // Add here list of php file(s) stored in justificativedocuments/core/boxes that contains a class to show a widget.
