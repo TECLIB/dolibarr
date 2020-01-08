f1817b2fdc8160461a4f5e3ff9a47ae999d0187a
Laurent Destailleur <eldy@destailleur.fr>
2020-01-08 19:27:20 +0100

    Fix

--- htdocs/justificativedocuments/justificativedocument_card.php
+++ htdocs/justificativedocuments/justificativedocument_card.php
@@ -126,8 +126,10 @@ if (empty($reshook))
     $backurlforlist = dol_buildpath('/justificativedocuments/justificativedocument_list.php', 1);
 
     if (empty($backtopage) || ($cancel && empty($id))) {
-    	if (empty($id) && (($action != 'add' && $action != 'create') || $cancel)) $backtopage = $backurlforlist;
-    	else $backtopage = dol_buildpath('/justificativedocuments/justificativedocument_card.php', 1).'?id='.($id > 0 ? $id : '__ID__');
+        if (empty($backtopage) || ($cancel && strpos($backtopage, '__ID__'))) {
+            if (empty($id) && (($action != 'add' && $action != 'create') || $cancel)) $backtopage = $backurlforlist;
+	    	else $backtopage = dol_buildpath('/justificativedocuments/justificativedocument_card.php', 1).'?id='.($id > 0 ? $id : '__ID__');
+        }
     }
     $triggermodname = 'JUSTIFICATIVEDOCUMENTS_JUSTIFICATIVEDOCUMENT_MODIFY';	// Name of trigger action code to execute when we modify record
 
