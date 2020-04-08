48fc12b5983f8459771f616041ba8c7c497ce9f6
Laurent Destailleur <eldy@destailleur.fr>
2020-04-08 14:29:26 +0200

    Update logo

--- htdocs/advanceddiscount/core/modules/modAdvancedDiscount.class.php
+++ htdocs/advanceddiscount/core/modules/modAdvancedDiscount.class.php
@@ -78,7 +78,7 @@ class modAdvancedDiscount extends DolibarrModules
 		// Name of image file used for this module.
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
-		$this->picto='generic';
+		$this->picto='advanceddiscount@advanceddiscount';
 
 		// Defined all module parts (triggers, login, substitutions, menus, css, etc...)
 		// for default path (eg: /advanceddiscount/core/xxxxx) (0=disable, 1=enable)
