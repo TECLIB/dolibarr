6b62b1559d86fb1f35475eb3b6046507a946e012
Laurent Destailleur <eldy@destailleur.fr>
2024-10-26 15:25:57 +0200

    Prepare for v21

--- htdocs/advanceddiscount/advanceddiscount_card.php
+++ htdocs/advanceddiscount/advanceddiscount_card.php
@@ -534,7 +534,11 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 		*/
 
 	    // Show links to link elements
-	    //$linktoelem = $form->showLinkToObjectBlock($object, null, array('advanceddiscount'));
+	    //$tmparray = $form->showLinkToObjectBlock($object, array(), array('advanceddiscount'), 1);
+	    //$linktoelem = $tmparray['linktoelem'];
+	    //$htmltoenteralink = $tmparray['htmltoenteralink'];
+	    //print $htmltoenteralink;
+
 	    //$somethingshown = $form->showLinkedObjectBlock($object, $linktoelem);
 
 
